7537ef2e616dc023d3dc9fc949e6675db11167fc
asukaaasaaa <132123244+asukaaasaaa@users.noreply.github.com>
2023-07-05 06:36:03 +0000

    [add]comment

--- app/controllers/public/comments_controller.rb
+++ app/controllers/public/comments_controller.rb
@@ -1,2 +1,20 @@
 class Public::CommentsController < ApplicationController
+  def create
+    post_tea = PostTea.find(params[:post_tea_id])
+    comment = current_customer.comments.new(comment_params)
+    comment.post_tea_id = post_tea.id
+    comment.save
+    redirect_to post_tea_path(post_tea)
+  end
+
+  def destroy
+    Comment.find(params[:id]).destroy
+    redirect_to post_tea_path(params[:post_tea_id])
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:comment)
+  end
 end
