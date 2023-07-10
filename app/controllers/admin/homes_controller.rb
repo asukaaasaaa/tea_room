f8f148a6361e876d27956327b8d1f613ffdc5529
asukaaasaaa <132123244+asukaaasaaa@users.noreply.github.com>
2023-07-10 02:02:22 +0000

    [add]admin/home

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -1,4 +1,13 @@
 class Admin::HomesController < ApplicationController
   def top
+    @customers = Customer.all
+
+    @post_teas = PostTea.all
+    @today_post_tea =  @post_teas.created_today
+    @yesterday_post_tea = @post_teas.created_yesterday
+    @this_week_post_tea = @post_teas.created_this_week
+    @last_week_post_tea = @post_teas.created_last_week
+    @comments = Comment.all
+
   end
 end
--- app/models/post_tea.rb
+++ app/models/post_tea.rb
@@ -4,20 +4,20 @@ class PostTea < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :favorited_customers, through: :favorites, source: :customer
-  
+
   has_one_attached :image
   def get_image
     (image.attached?) ? image : 'no_image.jpg'
   end
-  
+
   validates :genre_id, presence: true
   validates :title,presence:true
   validates :body,presence:true,length:{maximum:200}
-  
+
   def favorited_by?(customer)
     favorites.where(customer_id: customer.id).exists?
   end
-  
+
   # 検索方法分岐
   def self.looks(search, word)
     if search == "perfect_match"
@@ -32,5 +32,11 @@ class PostTea < ApplicationRecord
       @post_tea = PostTea.all
     end
   end
-  
+
+  scope :created_today, -> { where(created_at: Time.zone.now.all_day) }
+  scope :created_yesterday, -> { where(created_at: 1.day.ago.all_day) }
+  scope :created_this_week, -> { where(created_at: 6.day.ago.beginning_of_day..Time.zone.now.end_of_day) }
+  scope :created_last_week, -> { where(created_at: 2.week.ago.beginning_of_day..1.week.ago.end_of_day) }
+
+
 end
